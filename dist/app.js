0e50a0cfc02ca405bf96b5a601716654a08ab251
rohan <rohanrajgautam@gmail.com>
2020-06-15 09:57:30 +0545

    added https in the api

--- dist/app.js
+++ dist/app.js
@@ -4,7 +4,7 @@ function getJokes(e) {
   const number = document.querySelector('input[type="number"]').value;
   const xhr = new XMLHttpRequest();
 
-  xhr.open("GET", `http://api.icndb.com/jokes/random/${number}`, true);
+  xhr.open("GET", `https://api.icndb.com/jokes/random/${number}`, true);
 
   xhr.onload = function () {
     if (this.status === 200) {
